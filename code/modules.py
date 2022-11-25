23649ee3827aa4e77c10ad287efe8bf9ad2ccc56
TristanFaine <tristan.faine1@gmail.com>
2022-11-25 11:09:10 +0000

    Added modified version of AlexNet to available models

--- code/modules.py
+++ code/modules.py
@@ -1,5 +1,5 @@
 import torch.nn.functional as F
-from torch.nn import Module, Conv2d, MaxPool2d, Linear
+from torch.nn import Module, Conv2d, MaxPool2d, Linear, BatchNorm2d, Sequential, ReLU, Dropout
 
 class SegmentSelector(Module):
     def __init__(self, hiddencells = 100):
@@ -20,3 +20,53 @@ class SegmentSelector(Module):
         x = self.fc3(x)
         x = F.softmax(x, dim=1)
         return x
+
+
+class AlexNet(Module):
+    def __init__(self, num_classes=10):
+        super(AlexNet, self).__init__()
+        self.layer1 = Sequential(
+            Conv2d(1, 96, kernel_size=11, stride=4, padding=0),
+            BatchNorm2d(96),
+            ReLU()
+            )
+        self.layer2 = Sequential(
+            Conv2d(96, 384, kernel_size=5, stride=1, padding=2),
+            BatchNorm2d(384),
+            ReLU()
+            )
+        self.layer3 = Sequential(
+            Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(384),
+            ReLU())
+        self.layer4 = Sequential(
+            Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(384),
+            ReLU())
+        self.layer5 = Sequential(
+            Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(256),
+            ReLU()
+            )
+        self.fc = Sequential(
+            Dropout(0.5),
+            Linear(9216, 4096),
+            ReLU())
+        self.fc1 = Sequential(
+            Dropout(0.5),
+            Linear(4096, 4096),
+            ReLU())
+        self.fc2= Sequential(
+            Linear(4096, num_classes))
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        #out = self.layer3(out)
+        #out = self.layer4(out)
+        out = self.layer5(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
